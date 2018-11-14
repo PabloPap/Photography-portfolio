c5d4122e8deb5d06027ceb454736a1defa1130b6
Pavlos Papadopoulos <ppapad_33@yahoo.gr>
2018-11-14 18:14:10 +0200

    add dynamic year to footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,72 @@
-"use strict";
+/* eslint-disable func-names */
 
-var baguetteBox = require("baguettebox.js");
-require("picturefill");
-window.jQuery = require("jquery");
+'use strict';
 
-(function ($) {
-    $(function () {
-        // Avoid `console` errors in browsers that lack a console.
-        var method;
-        var noop = function () {};
-        var methods = [
-            "assert", "clear", "count", "debug", "dir", "dirxml", "error",
-            "exception", "group", "groupCollapsed", "groupEnd", "info", "log",
-            "markTimeline", "profile", "profileEnd", "table", "time", "timeEnd",
-            "timeline", "timelineEnd", "timeStamp", "trace", "warn"
-        ];
-        var length = methods.length;
-        var console = (window.console = window.console) || (window.console = {});
+var baguetteBox = require('baguettebox.js');
+require('picturefill');
+window.jQuery = require('jquery');
 
-        while (length) {
-            length -= length;
-            method = methods[length];
+(function($) {
+  $(function() {
+    // Avoid `console` errors in browsers that lack a console.
+    var method;
+    var noop = function() {};
+    var methods = [
+      'assert',
+      'clear',
+      'count',
+      'debug',
+      'dir',
+      'dirxml',
+      'error',
+      'exception',
+      'group',
+      'groupCollapsed',
+      'groupEnd',
+      'info',
+      'log',
+      'markTimeline',
+      'profile',
+      'profileEnd',
+      'table',
+      'time',
+      'timeEnd',
+      'timeline',
+      'timelineEnd',
+      'timeStamp',
+      'trace',
+      'warn',
+    ];
+    var length = methods.length;
 
-            // Only stub undefined methods.
-            if (!console[method]) {
-                console[method] = noop;
-            }
-        }
+    // eslint-disable-next-line no-self-assign
+    var console = (window.console = window.console) || (window.console = {});
 
-        baguetteBox.run(".gallery", {
-            overlayBackgroundColor: "rgba(57,57,57,0.8)"
-        });
+    while (length) {
+      length -= length;
+      method = methods[length];
 
-        $("input,textarea").focus(function () {
-            $(this).data("placeholder", $(this).attr("placeholder")).attr("placeholder", "");
-        }).blur(function () {
-            $(this).attr("placeholder", $(this).data("placeholder"));
-        });
+      // Only stub undefined methods.
+      if (!console[method]) {
+        console[method] = noop;
+      }
+    }
+
+    baguetteBox.run('.gallery', {
+      overlayBackgroundColor: 'rgba(57,57,57,0.8)',
     });
-}(jQuery));
+
+    $('input,textarea')
+      .focus(function() {
+        $(this)
+          .data('placeholder', $(this).attr('placeholder'))
+          .attr('placeholder', '');
+      })
+      .blur(function() {
+        $(this).attr('placeholder', $(this).data('placeholder'));
+      });
+
+    // add dynamic year to footer
+    $('#year').text(new Date().getFullYear());
+  });
+})(jQuery);
